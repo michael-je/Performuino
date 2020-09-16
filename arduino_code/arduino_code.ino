1a7d8e91611043edb4055ab6cf92612dfdd11990
Michael Joseph <michaeljoseph195@gmail.com>
2020-09-16 20:44:48 +0200

    added comments to arduino code

--- arduino_code/arduino_code.ino
+++ arduino_code/arduino_code.ino
@@ -1,14 +1,13 @@
-#include <Wire.h>
-#include <Adafruit_MCP4725.h>
-#include <IRremote.h>
+#include <Wire.h>               // I2C communication protocol for DAC
+#include <Adafruit_MCP4725.h>   // library for communicating with the DAC
+#include <IRremote.h>           // library for using the infrared LED as a remote control for the projector
 
-Adafruit_MCP4725 dac;
-IRsend irsend;
+Adafruit_MCP4725 dac;           // construct a dac object
+IRsend irsend;                  // construct an infrared commication object
 
-int LEDPin = 2;
 int IRPin = 3;
 int solenoidPin = 9;
-int lowCapPin = 5;
+int lowCapPin = 5;              // open/close the synth circuit to a 100uF capacitor. when open it greatly lowers the oscillator frequency
 int bloopPin = 6;
 int lampPin = 7;
 int motorPin = 10;
@@ -21,6 +20,7 @@ int pwm_value;
 int command;
 int motor_pwm_value;
 
+// lists used by the IR emitter to send specific commands to the projector
 unsigned int projector_power[68] = {9050,4500,550,550,600,550,550,600,550,550,550,600,550,600,550,550,550,600,550,1700,550,1700,550,1700,550,1750,550,1700,550,1700,550,1700,550,1700,550,1700,600,550,550,1700,550,600,550,550,600,550,550,1700,550,600,550,600,550,1700,550,550,550,1700,600,1700,550,1700,550,600,550,1700,550,};
 unsigned int projector_pink[68] = {8950,4550,550,550,550,600,500,650,550,550,550,600,550,550,550,600,550,600,550,1700,550,1750,500,1700,550,1700,550,1750,550,1700,550,1700,550,1750,500,1750,500,600,550,600,550,1700,550,550,550,600,550,600,550,550,550,600,550,1750,500,1700,550,600,550,1750,500,1700,550,1750,500,1750,550,};
 unsigned int projector_blue[68] = {9050,4500,550,600,550,550,550,600,550,550,550,600,550,600,550,550,550,600,550,1700,550,1750,500,1750,500,1750,500,1750,550,1700,550,1700,550,1750,500,1750,500,1750,550,550,550,600,550,550,550,600,550,1750,500,600,550,550,550,600,550,1750,500,1750,500,1750,550,1700,550,550,550,1750,550,};
@@ -37,9 +37,9 @@ void setup() {
   pinMode(bloopPin, OUTPUT);
   pinMode(photoResistorPin, INPUT);
 
-  Serial.begin(9600);
-  dac.begin(0x60);
-  dac.setVoltage(0, false);
+  Serial.begin(9600);                       // enable the serial connection with python script
+  dac.begin(0x60);                          // enable the DAC module
+  dac.setVoltage(0, false);                 // intialise DAC output to 0
   digitalWrite(lowCapPin, HIGH);
   digitalWrite(motorPin, LOW);
   digitalWrite(bloopPin, LOW);
@@ -47,6 +47,8 @@ void setup() {
 }
 
 void loop() {
+  // excecute commands based on received serial information from python script
+  
   if (Serial.available()) {
     Serial.print("peek: ");
     Serial.println(Serial.peek());
